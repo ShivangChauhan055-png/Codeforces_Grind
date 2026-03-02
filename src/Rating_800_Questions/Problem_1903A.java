4ac6edf3913d9e5fc88015a31cad50e2f43b14f2
Shivang Chauhan <shivangchauhan055@gmail.com>
2026-03-02 12:12:45 +0530

    Problem_1930A

--- src/Rating_800_Questions/Problem_1903A.java
+++ src/Rating_800_Questions/Problem_1903A.java
@@ -1,7 +1,24 @@
 package Rating_800_Questions;
 
+import java.util.*;
 public class Problem_1903A {
     public static void main(String[] args) {
-
+        Scanner sc  = new Scanner(System.in);
+        int t  = sc.nextInt();
+        while(t-- >0) {
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+            long[] arr = new long[(int) n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextLong();
+            }
+            long[] copy_arr = Arrays.copyOf(arr, arr.length);
+            Arrays.sort(copy_arr);
+            if(Arrays.equals(copy_arr,arr) || k>1 ){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
     }
 }
