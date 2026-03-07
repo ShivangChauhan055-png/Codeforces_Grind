1dba27218aac64714358a7511d38ac409427e3c8
Shivang Chauhan <shivangchauhan055@gmail.com>
2026-03-07 22:17:55 +0530

    Problem_1877A

---
+++ src/Rating_800_Questions/Problem_1877A.java
@@ -0,0 +1,18 @@
+package Rating_800_Questions;
+
+import java.util.Scanner;
+
+public class Problem_1877A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t--> 0){
+            int n = sc.nextInt();
+            int sum =0;
+            for (int i = 0; i < n-1; i++) {
+                sum+=sc.nextInt();
+            }
+            System.out.println(-1*sum);
+        }
+    }
+}
