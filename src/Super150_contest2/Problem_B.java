4e4ab39325ed278b1458196f7938c91592cbd08d
Shivang Chauhan <shivangchauhan055@gmail.com>
2026-04-04 15:12:42 +0530

    add

--- src/Super150_contest2/Problem_B.java
+++ src/Super150_contest2/Problem_B.java
@@ -7,17 +7,17 @@ public class Problem_B {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         int a = 0;
-        int b = 0;
+        int ans = 0;
         for(char ch : s.toCharArray()) {
             if(ch == '(') {
                 a++;
             } else {
                 if(a > 0) {
                     a--;
-                    b++;
+                    ans++;
                 }
             }
         }
-        System.out.println(b * 2);
+        System.out.println(ans * 2);
     }
 }
