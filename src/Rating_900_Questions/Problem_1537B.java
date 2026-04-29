85eec276406eefb8c53208575e52b16d602be71d
Shivang Chauhan <shivangchauhan055@gmail.com>
2026-04-29 13:47:58 +0530

    900 rated

---
+++ src/Rating_900_Questions/Problem_1537B.java
@@ -0,0 +1,17 @@
+package Rating_900_Questions;
+
+import java.util.Scanner;
+
+public class Problem_1537B {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0){
+            long x = sc.nextLong();
+            long y = sc.nextLong();
+            long i = sc.nextLong();
+            long j = sc.nextLong();
+            System.out.println(1+" "+1+" "+x+" "+y);
+        }
+    }
+}
