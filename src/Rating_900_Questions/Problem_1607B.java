6a33d05bd95887dcf49d8bbdf141e77e583bee2d
Shivang Chauhan <shivangchauhan055@gmail.com>
2026-08-20 23:15:02 +0530

    900

--- src/Rating_900_Questions/Problem_1607B.java
+++ src/Rating_900_Questions/Problem_1607B.java
@@ -6,15 +6,21 @@ public class Problem_1607B{
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t-->0){
-            long n = sc.nextLong();
             long x = sc.nextLong();
+            long n = sc.nextLong();
             if(n%4==0) System.out.println(x);
-            else if(n%4==1){
-                System.out.println((x%2==0) ? x-n : x+n);
-            } else if (n%4==2) {
-                System.out.println((x%2==0) ? x+1 : x-1);
-            } else if (n%4==3) {
-                System.out.println((x%2==0) ? x+n+1 : x-n-1);
+            else if (n%4==1) {
+                if(x%2==0) x = x-n;
+                else x = x+n;
+                System.out.println(x);
+            }else if(n%4==2){
+                if(x%2==0) x= x+1;
+                else x = x-1;
+                System.out.println(x);
+            }else{
+                if(x%2==0) x = x+n+1;
+                else x = x-n-1;
+                System.out.println(x);
             }
         }
     }
